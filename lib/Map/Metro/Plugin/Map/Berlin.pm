d493b688bd1d855bf7c6319e1cfe8e2023854913
Slaven Rezic <slaven@rezic.de>
2020-11-01 12:00:09 +0100

    network changes in Berlin

--- lib/Map/Metro/Plugin/Map/Berlin.pm
+++ lib/Map/Metro/Plugin/Map/Berlin.pm
@@ -3,7 +3,7 @@
 #
 # Author: Slaven Rezic
 #
-# Copyright (C) 2014,2015,2017,2018,2019 Slaven Rezic. All rights reserved.
+# Copyright (C) 2014,2015,2017,2018,2019,2020 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -14,7 +14,7 @@
 {
     package Map::Metro::Plugin::Map::Berlin;
 
-    our $VERSION = '2019.1215';
+    our $VERSION = '2020.1101';
 
     use Moose;
     use File::ShareDir 'dist_dir';
